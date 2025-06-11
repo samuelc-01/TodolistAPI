922fed7946314913d7d45e3fdbf25f354282ca22
dev <94959646+samuelc-01@users.noreply.github.com>
2025-06-11 12:28:08 -0300

    add create a module for protected jwt

--- app/core/security.py
+++ app/core/security.py
@@ -26,3 +26,4 @@ def create_access_token(data: dict):
     expire = datetime.utcnow() + timedelta(minutes=EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -5,12 +5,14 @@ from app.core.security import verify_password, create_access_token
 from app.db.database import get_db
 from app.schemas.models import User
 from app.schemas.token import Token
+from app.core.security import get_password_hash
 
 #test commit vscode
 router = APIRouter()
 
 @router.post("/login", response_model=Token)
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    
     user = db.query(User).filter(User.email== form_data.username).first()
 
     if not user:
@@ -21,3 +23,4 @@ def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depend
 
     token = create_access_token({"sub": user.email})
     return {"access_token": token, "token_type": "bearer"}
+
