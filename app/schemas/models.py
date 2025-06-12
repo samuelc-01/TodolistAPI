7ce067bdf4b1f04e92776c89af511e9efc7cf2e1
dev <94959646+samuelc-01@users.noreply.github.com>
2025-06-12 11:32:56 -0300

    feat(todo): define todo model with FK to user

--- app/schemas/models.py
+++ app/schemas/models.py
@@ -10,3 +10,14 @@ class User(Base):
     password = Column(String)
 
     
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from app.db.database import Base
+
+class Todo(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, nullable=True)
+    completed = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
